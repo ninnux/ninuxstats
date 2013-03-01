690451fb8513b42e1731a351592812935d9f6d0e
Nino <nino@ninux.org>
2013-03-01 10:01:10 +0000

    risolto conflitto

--- genera.php
+++ genera.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 $app_path="/root/ninuxstats/";
 $ip=$_GET['ip'];
 if (preg_match( '/^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:[.](?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$/', $ip)) {
@@ -9,12 +8,6 @@ if (preg_match( '/^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:[.](?:25[0-5]|2[0-4]\
 	print "<img src='graphs/".$ip."_7d.png'/>";
 	system($app_path."/genera.sh ".$ip." 1m > /dev/null");
 	print "<img src='graphs/".$ip."_1m.png'/>";
-=======
-$ip=$_GET['ip'];
-if (preg_match( '/^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:[.](?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$/', $ip)) {
-	system("/root/ninuxstats/genera.sh ".$ip." > /dev/null");
-	print "<img src='graphs/".$ip.".png'/>";
->>>>>>> db9b3a20cd467716555704b34da3683bbb0f7dfe
 }else{
 print "fanculo!";
 }

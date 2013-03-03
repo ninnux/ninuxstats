e69500d6b7f887cf43cd6b79fbed2e3c8e59ebca
Nino <nino@ninux.org>
2013-03-03 19:55:55 +0000

    vediamo se va solo sugli ip della classe 10

--- main.sh
+++ main.sh
@@ -5,5 +5,5 @@ cd $APP_DIR
 for i in $(route -n | grep 172. | grep 255.255.255.255 | awk '{print $1}'); do 
 	echo $i
 	./rrd2.sh $i > rrd.log 2> rrd.err& 
-	python snmptools.py $i > rrdpy.log 2> rrdpy.err& 
+	#python snmptools.py $i > rrdpy.log 2> rrdpy.err 
 done

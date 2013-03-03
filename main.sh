3ecf16478622724d1fdaecf6fbfa36e6d012d9b9
Nino <nino@ninux.org>
2013-03-03 19:43:40 +0000

    main lancia entrambe le versioni

--- main.sh
+++ main.sh
@@ -2,7 +2,8 @@
 echo "afissemammita" >> /root/ninuxstats/rrds/prova.txt
 APP_DIR="/root/ninuxstats/"
 cd $APP_DIR
-for i in $(route -n | grep 172.16. | grep 255.255.255.255 | awk '{print $1}'); do 
+for i in $(route -n | grep 172. | grep 255.255.255.255 | awk '{print $1}'); do 
 	echo $i
 	./rrd2.sh $i > rrd.log 2> rrd.err& 
+	python snmptools.py $i > rrdpy.log 2> rrdpy.err& 
 done

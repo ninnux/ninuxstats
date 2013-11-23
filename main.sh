e1dc3a583e04764aeae3b951694bd51d91ff2b60
Nino <nino@ninux.org>
2013-11-23 15:11:30 +0000

    append ai log

--- main.sh
+++ main.sh
@@ -2,8 +2,10 @@
 echo "afissemammita" >> /root/ninuxstats/rrds/prova.txt
 APP_DIR="/root/ninuxstats/"
 cd $APP_DIR
+cat /dev/null > rrd.log
+cat /dev/null > rrd.err
 for i in $(route -n | grep 172. | grep 255.255.255.255 | awk '{print $1}'); do 
 	echo $i
-	./rrd2.sh $i > rrd.log 2> rrd.err& 
+	./rrd2.sh $i >> rrd.log 2>> rrd.err& 
 	#python snmptools.py $i > rrdpy.log 2> rrdpy.err 
 done

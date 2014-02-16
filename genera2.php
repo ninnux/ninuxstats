7b7222a31cdec71e35155fbbd0b90fdd7ad62b17
Nino <nino@ninux.org>
2014-02-16 22:33:12 +0000

    sembra girare

--- genera2.php
+++ genera2.php
@@ -1,13 +1,17 @@
 <?php
 $app_path="/root/ninuxstats/";
 $ip_iface=$_GET['ip'];
-$ip_iface_arr=explode("_",$ip1);
-$ip1=ip_iface_arr[0];
-$iface=ip_iface_arr[0];
-print $ip1." ".$iface;
-exec("echo /mid | nc 127.0.0.1 2006 |grep ".$ip1." | awk -F"\t" '{print $1}'",$result);
+$ip_iface_arr=explode("_",$ip_iface);
+$ip1=$ip_iface_arr[0];
+$iface=$ip_iface_arr[1];
+//print $ip1." ".$iface;
+exec("echo /mid | nc 127.0.0.1 2006 |grep ".$ip1." | awk -F\"\\t\" '{print $1}'",$result);
 $main_ip=$result[0];
-$ip=$main_ip."_".$iface;
+if($main_ip!=""){
+	$ip=$main_ip."_".$iface;
+}else{
+	$ip=$ip_iface;
+}
 if (preg_match( '/^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:[.](?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}\_.*$/', $ip)) {
 	system($app_path."genera2.sh ".$ip." 2d > /dev/null");
 	print "<img src='graphs/".$ip."_2d.png'/>";

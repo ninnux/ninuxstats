8a0ed0666c5768fee93ae736dabe2f8930361621
Nino <nino@ninux.org>
2015-11-16 11:06:59 +0000

    stampa singola immagine via web

--- genera2.php
+++ genera2.php
@@ -16,12 +16,25 @@ if($main_ip!=""){
 if (preg_match( '/^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:[.](?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}\_.*$/', $ip)) {
 	//print "file:".$app_path.$rrd_path.$ip."\n";
 	if(file_exists($app_path.$rrd_path.$ip.".rrd")){
-		system($app_path."genera2.sh ".escapeshellcmd($ip)." 2d > /dev/null");
-		print "<img src='graphs/".$ip."_2d.png'/>";
-		system($app_path."/genera2.sh ".escapeshellcmd($ip)." 7d > /dev/null");
-		print "<img src='graphs/".$ip."_7d.png'/>";
-		system($app_path."/genera2.sh ".escapeshellcmd($ip)." 1m > /dev/null");
-		print "<img src='graphs/".$ip."_1m.png'/>";
+		if(isset($_GET['img'])){
+			$img=$_GET['img'];
+			if($img=="2d" or $img=="7d" or $img=="1m" or $img=="2y"){
+				system($app_path."genera2.sh ".escapeshellcmd($ip)." ".$img." > /dev/null");
+				$file='graphs/'.$ip.'_'.$img.'.png';
+				header('Content-Type:image/png');
+				header('Content-Length: '.filesize($file));
+				readfile($file);
+			}
+		}else{
+			system($app_path."genera2.sh ".escapeshellcmd($ip)." 2d > /dev/null");
+			print "<img src='graphs/".$ip."_2d.png'/>";
+			system($app_path."/genera2.sh ".escapeshellcmd($ip)." 7d > /dev/null");
+			print "<img src='graphs/".$ip."_7d.png'/>";
+			system($app_path."/genera2.sh ".escapeshellcmd($ip)." 1m > /dev/null");
+			print "<img src='graphs/".$ip."_1m.png'/>";
+			system($app_path."/genera2.sh ".escapeshellcmd($ip)." 2y > /dev/null");
+			print "<img src='graphs/".$ip."_2y.png'/>";
+		}
 	}else{
 		$command="find ".$app_path.$rrd_path." -name \"".escapeshellcmd($ip)."*\" -print";
 		//print $command."\n";
